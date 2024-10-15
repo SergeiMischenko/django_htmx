6ca7fff9a56f3ecc5db6613dd2e2b2d383332e05
Sergey <main0serg@gmail.com>
2024-10-15 12:50:27 +0600

    added form for create book

--- books/views.py
+++ books/views.py
@@ -2,10 +2,20 @@ from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_http_methods
 
+from .forms import BookCreateForm
 from .models import Book
 
 
 @require_http_methods(["GET"])
 def book_list(request):
     book_list = Book.objects.all()
-    return render(request, "base.html", {"book_list": book_list})
+    form = BookCreateForm(auto_id=False)
+    return render(request, "base.html", {"book_list": book_list, "form": form})
+
+
+@require_http_methods(["POST"])
+def create_book(request):
+    form = BookCreateForm(request.POST)
+    if form.is_valid:
+        book = form.save()
+    return render(request, "partial_book_detail.html", {"book": book})
