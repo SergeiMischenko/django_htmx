98f9ac179d9e97a727d66ec62b93bfd1ce05398a
Sergey <main0serg@gmail.com>
2024-10-15 13:20:39 +0600

    added htmx filters for th table

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import (book_detail, book_list, create_book, delete_book,
-                    update_book_details, update_book_status)
+from .views import (book_detail, book_list, book_list_sort, create_book,
+                    delete_book, update_book_details, update_book_status)
 
 urlpatterns = [
     path("", book_list, name="book_list"),
@@ -12,4 +12,5 @@ urlpatterns = [
     path("book_detail/<int:pk>/", book_detail, name="book_detail"),
     path("delete_book/<int:pk>/", delete_book, name="delete_book"),
     path("update_book_status/<int:pk>/", update_book_status, name="update_book_status"),
+    path("book_list_sort/<filter>/<direction>/", book_list_sort, name="book_list_sort"),
 ]
