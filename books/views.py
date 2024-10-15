54a7aeaa15b54b5e93fc8668adef94e0a93aebe0
Sergey <main0serg@gmail.com>
2024-10-15 13:10:33 +0600

    added htmx script for put request

--- books/views.py
+++ books/views.py
@@ -1,8 +1,7 @@
-from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_http_methods
 
-from .forms import BookCreateForm
+from .forms import BookCreateForm, BookEditForm
 from .models import Book
 
 
@@ -18,4 +17,24 @@ def create_book(request):
     form = BookCreateForm(request.POST)
     if form.is_valid:
         book = form.save()
+    return render(request, "partial_book_detail.html", {"book": book})
+
+
+def update_book_details(request, pk):
+    book = Book.objects.get(pk=pk)
+    if request.method == "POST":
+        form = BookEditForm(request.POST, instance=book)
+        if form.is_valid():
+            book = form.save()
+            return render(request, "partial_book_detail.html", {"book": book})
+    else:
+        form = BookEditForm(instance=book)
+    return render(
+        request, "partial_book_update_form.html", {"book": book, "form": form}
+    )
+
+
+@require_http_methods(["GET"])
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
     return render(request, "partial_book_detail.html", {"book": book})
